8c0c7900afa6b49de454b6841657a94a64e48ae4
Castro-007 <sammythompson202@gmail.com>
2025-06-16 20:43:19 +0100

    added mirae asset

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ProjCards from './ProjCards'
-import ELITE from '../assets/images/eliteshott1.png'
+import Mirae from '../assets/images/miraeasset.png'
 import LOPUBLI from '../assets/images/lopubli.png'
 import RETAIL from '../assets/images/Retailallhome.png'
 import JSCLOCK from '../assets/images/jsclock.png'
@@ -16,19 +16,19 @@ const Project = () => {
         
 
           <ProjCards
-          imgs= ""
-          link =""
-          text =""
+          imgs= {Mirae}
+          link ="https://mirae-asset.onrender.com/"
+          text ="MiRAE ASSET - Built a smaple Landing page for Mirae Asset that provides information on financial services and products"
           />
           <ProjCards
           imgs={LOPUBLI}
           link ="https://lopubliication.onrender.com/"
-          text ="LOPUBLICATIONS - PUBLISHING WEBPAGE"
+          text ="LOPUBLICATIONS - PUBLISHING WEBSITE "
           />
           <ProjCards
           imgs={RETAIL}
           link ="https://retailall.onrender.com/"
-          text ="RETAIL&ALL - An e-commerce webpage that elevates fashion"
+          text ="RETAIL&ALL - A Fashion e-commerce webapp that elevates fashion"
           />
           <ProjCards
           imgs={Nike}
